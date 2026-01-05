440938d91b65506dd6d6b6c43eb10196389e9bae
Tnkata <timnkata@gmail.com>
2026-01-04 17:01:48 -0700

    Removed .* usage

--- src/main/java/com/personal/budgetApp/Controller/UserController.java
+++ src/main/java/com/personal/budgetApp/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.personal.budgetApp.Controller;
 
-import static com.personal.budgetApp.Constants.ServiceConstants.*;
+import static com.personal.budgetApp.Constants.ServiceConstants.CREATE_USER_ENDPOINT;
+import static com.personal.budgetApp.Constants.ServiceConstants.GET_USER_ENDPOINT;
+import static com.personal.budgetApp.Constants.ServiceConstants.USER_CONTROLLER_SCOPE;
 
 import com.personal.budgetApp.Model.Request.User.CreateUserRequest;
 import com.personal.budgetApp.Model.Response.CreateUserResponse;
@@ -13,7 +15,13 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
